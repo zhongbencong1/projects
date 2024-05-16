5c121770ed995d568d81a7466f262af1307cec65
tofaker <tofaker@163.com>
2024-05-16 20:05:29 +0800

    integration rocketmq

--- project-repository-microservice/project-repository-kafka/src/main/java/com/faker/project/controller/KafkaController.java
+++ project-repository-microservice/project-repository-kafka/src/main/java/com/faker/project/controller/KafkaController.java
@@ -4,7 +4,6 @@ import com.faker.project.kafka.KafkaProducer;
 import com.faker.project.vo.KafkaMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ project-repository-microservice/project-repository-kafka/src/main/java/com/faker/project/rocket/RocketMQProducer.java
@@ -0,0 +1,13 @@
+package com.faker.project.rocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 通过 RocketMQ 发送消息
+ */
+@Slf4j
+@Component
+public class RocketMQProducer {
+    
+}
