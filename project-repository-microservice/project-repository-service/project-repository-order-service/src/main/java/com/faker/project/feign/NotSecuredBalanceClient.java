0047aa87629da9b93c2bfed27a3d394b77dd856e
tofaker <tofaker@163.com>
2024-06-11 22:51:56 +0800

    Project order

--- project-repository-microservice/project-repository-service/project-repository-order-service/src/main/java/com/faker/project/feign/NotSecuredBalanceClient.java
+++ project-repository-microservice/project-repository-service/project-repository-order-service/src/main/java/com/faker/project/feign/NotSecuredBalanceClient.java
@@ -8,18 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * 用户账户服务 Feign 接口: contextId:client唯一id, value:调用的微服务的name.
- * NotSecuredBalanceClient: 不安全的调用, 调用失败需要抛出异常而非吞掉异常
+ * 用户账户服务Feign接口: contextId:client唯一id, value:调用的微服务的name.
+ * NotSecuredBalanceClient: 不安全的调用, 调用失败需要抛出异常而非吞掉异常: 用于事务
  * */
-@FeignClient(
-        contextId = "NotSecuredBalanceClient",
-        value = "project-repository-account-service"
-)
+@FeignClient(contextId = "NotSecuredBalanceClient", value = "project-repository-account-service")
 public interface NotSecuredBalanceClient {
 
-    @RequestMapping(
-            value = "/project-account-service/balance/deduct-balance",
-            method = RequestMethod.PUT
-    )
+    @RequestMapping(value = "/project-account-service/balance/deduct-balance", method = RequestMethod.PUT)
     CommonResponse<BalanceInfo> deductBalance(@RequestBody BalanceInfo balanceInfo);
 }
