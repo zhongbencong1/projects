97e8a2de3669ea236bcb027a40c494b7325b40e7
faker <tofaker@163.com>
2024-06-23 16:09:55 +0800

    sentinel 集成 gateway, 不同限流方式: 文件\nacos\硬编码

--- project-repository-microservice/project-repository-gateway/src/main/java/com/faker/project/config/SentinelGatewayConfiguration.java
+++ project-repository-microservice/project-repository-gateway/src/main/java/com/faker/project/config/SentinelGatewayConfiguration.java
@@ -1,11 +1,18 @@
 package com.faker.project.config;
 
 import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -13,7 +20,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.ViewResolver;
 
 import javax.annotation.PostConstruct;
@@ -31,6 +42,10 @@ public class SentinelGatewayConfiguration {
 
     /** HTTP 请求和响应数据的编解码配置 */
     private final ServerCodecConfigurer serverCodecConfigurer;
+
+    /** api分组常量 */
+    public static final String API_GROUP_ONE = "nacos-client-api-1";
+    public static final String API_GROUP_TWO = "nacos-client-api-2";
 
     /** 构造方法 */
     public SentinelGatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider, ServerCodecConfigurer serverCodecConfigurer) {
@@ -59,6 +74,8 @@ public class SentinelGatewayConfiguration {
         log.info("SentinelGatewayConfiguration doInit start");
         // 加载网关限流规则
         initGatewayRules();
+        // 加载自定义限流异常处理器
+        initBlockHandler();
         log.info("SentinelGatewayConfiguration doInit finish");
     }
 
@@ -68,7 +85,7 @@ public class SentinelGatewayConfiguration {
         GatewayFlowRule rule = new GatewayFlowRule()
             // 指定限流模式, 根据 route_id 做限流, 默认的模式
             .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_ROUTE_ID)
-            // 指定 route_id -> serviceId: 微服务name
+            // 指定 route_id -> serviceId: 微服务name 即将整个微服务的接口添加限流
             .setResource("project-repository-nacos-client")
             // 按照 QPS 限流
             .setGrade(RuleConstant.FLOW_GRADE_QPS)
@@ -77,11 +94,76 @@ public class SentinelGatewayConfiguration {
             .setCount(3);
 
         Set<GatewayFlowRule> rules = new HashSet<>();
-        rules.add(rule);
+//        rules.add(rule);
+
+        // 限流分组, Sentinel 先去找规则定义(GatewayFlowRule实例), 再去找规则中定义的分组: 根据分组名称(resource属性)找分组(ApiDefinition实例)
+        rules.add(new GatewayFlowRule(API_GROUP_ONE).setCount(3).setIntervalSec(60));
+        rules.add(new GatewayFlowRule(API_GROUP_TWO).setCount(1).setIntervalSec(60));
 
         // 加载到网关中
         GatewayRuleManager.loadRules(rules);
+        // 加载限流分组
+        initCustomizedApis();
     }
 
+    /** 自定义限流异常处理器 */
+    private void initBlockHandler() {
+        // 自定义 BlockRequestHandler
+        BlockRequestHandler blockRequestHandler = (serverWebExchange, throwable) -> {
+            log.error("SentinelGatewayConfiguration initBlockHandler trigger gateway sentinel rule");
+            JSONObject result = new JSONObject();
+            result.put("code", String.valueOf(HttpStatus.TOO_MANY_REQUESTS.value()));
+            result.put("message", HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+            result.put("route", "project-repository-nacos-client");
 
+            return ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(result));
+        };
+
+        // 设置自定义限流异常处理器
+        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
+    }
+
+    /**
+     * 硬编码网关限流分组
+     * 1. 最大限制 == 微服务级别的限制(相当于没有分组)
+     * 2. 具体的分组
+     */
+    private void initCustomizedApis() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+
+        // nacos-client-api 组, 最大的限制== 微服务级别的限制(相当于没有分组)
+        ApiDefinition api = new ApiDefinition("nacos-client-api").setPredicateItems(
+                new HashSet<ApiPredicateItem>() {
+                    {
+                        // 模糊匹配 /repository/project-repository-nacos-client 及其子路径的所有请求; 匹配方式:前缀匹配
+                        add(new ApiPathPredicateItem().setPattern("/repository/project-repository-nacos-client/**")
+                                .setMatchStrategy(SentinelGatewayConstants.URL_MATCH_STRATEGY_PREFIX));
+                    }
+                });
+
+        // API分组1
+        ApiDefinition api1 = new ApiDefinition(API_GROUP_ONE).setPredicateItems(
+                new HashSet<ApiPredicateItem>() {
+                    {
+                        // 精确匹配url: /repository/project-repository-nacos-client/nacos-client/service_instance
+                        add(new ApiPathPredicateItem().setPattern("/repository/project-repository-nacos-client/nacos-client/service_instance"));
+                    }
+                });
+
+        // API分组2
+        ApiDefinition api2 = new ApiDefinition(API_GROUP_TWO).setPredicateItems(
+                new HashSet<ApiPredicateItem>() {
+                    {
+                        // 精确匹配url: /repository/project-repository-nacos-client/nacos-client/project-config
+                        add(new ApiPathPredicateItem().setPattern("/repository/project-repository-nacos-client/nacos-client/project-config"));
+                    }
+                });
+
+        definitions.add(api1);
+        definitions.add(api2);
+
+        // 加载限流分组
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+    }
 }
